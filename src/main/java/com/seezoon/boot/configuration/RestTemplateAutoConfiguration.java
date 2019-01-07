966698211a1622715818b0ddabfae117b8f765fd
hdf <hdf@qq.com>
2019-01-07 23:00:33 +0800

    restTemplate

--- src/main/java/com/seezoon/boot/configuration/RestTemplateAutoConfiguration.java
+++ src/main/java/com/seezoon/boot/configuration/RestTemplateAutoConfiguration.java
@@ -1,25 +1,102 @@
 package com.seezoon.boot.configuration;
 
 import java.nio.charset.StandardCharsets;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import com.seezoon.boot.common.http.HttpClientConfig;
+import com.seezoon.boot.common.http.HttpClientIdleConnectionMonitor;
 
 @Configuration
 public class RestTemplateAutoConfiguration {
 
-	public static int DEFAULT_READ_TIMEOUT = 5000;
-	public static int DEFAULT_CONNECT_TIMEOUT = 5000;
+	@Autowired
+	private HttpClientConfig httpClientConfig;
 	@Bean
 	public  RestTemplate restTemplate() {
-		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory(); 
-		factory.setReadTimeout(DEFAULT_READ_TIMEOUT);
-		factory.setConnectTimeout(DEFAULT_CONNECT_TIMEOUT);
+		ClientHttpRequestFactory factory = clientHttpRequestFactory(); 
 		RestTemplate restTemplate = new RestTemplate(factory);
 		restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
 		return restTemplate;
 	}
+	@Bean
+    public HttpClientConnectionManager poolingConnectionManager() {
+		SSLContext sslContext = null;
+		try {
+			sslContext = SSLContexts.custom().loadTrustMaterial(null, new TrustStrategy() {
+				@Override
+				public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+					return false;
+				}
+			}).build();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext,
+				NoopHostnameVerifier.INSTANCE);
+		Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+				.register("http", PlainConnectionSocketFactory.getSocketFactory()).register("https", sslSocketFactory)
+				.build();
+		PoolingHttpClientConnectionManager poolingConnectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+		poolingConnectionManager.setMaxTotal(httpClientConfig.getMaxTotal()); // 连接池最大连接数  
+		poolingConnectionManager.setDefaultMaxPerRoute(httpClientConfig.getMaxPerRoute()); // 每个主机的并发
+		// 长连接
+		poolingConnectionManager.setDefaultSocketConfig(SocketConfig.custom().setSoTimeout(httpClientConfig.getSocketTimeout()).setSoKeepAlive(true).build());
+				// 连接不活跃多久检查毫秒 并不是100 % 可信
+		poolingConnectionManager.setValidateAfterInactivity(httpClientConfig.getValidateAfterInactivity());
+		// 空闲扫描线程
+		HttpClientIdleConnectionMonitor.registerConnectionManager(poolingConnectionManager, httpClientConfig);
+		return poolingConnectionManager;
+    }
+    
+    @Bean
+    public HttpClientBuilder httpClientBuilder() {
+		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+	    //设置HTTP连接管理器
+		httpClientBuilder.setConnectionManager(poolingConnectionManager());
+		RequestConfig requestConfig = RequestConfig.custom()
+				//.setProxy(proxy)
+				.setConnectionRequestTimeout(httpClientConfig.getConnectionRequestTimeout())// 获取连接等待时间
+				.setConnectTimeout(httpClientConfig.getConnectTimeout())// 连接超时
+				.setSocketTimeout(httpClientConfig.getSocketTimeout())// 获取数据超时
+				.build();
+		httpClientBuilder.setDefaultRequestConfig(requestConfig)
+		.setUserAgent(httpClientConfig.getUserAgent())
+		.disableContentCompression().disableAutomaticRetries()
+		.setConnectionTimeToLive(httpClientConfig.getConnTimeToLive(), TimeUnit.MILLISECONDS)// 连接最大存活时间
+		.setRetryHandler(new DefaultHttpRequestRetryHandler(httpClientConfig.getRetyTimes(), true));// 重试次数
+		return httpClientBuilder;
+    }
+    
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory() { 
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setHttpClient(httpClientBuilder().build());
+		return clientHttpRequestFactory;
+    }
 }
